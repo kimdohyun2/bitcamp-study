daee60de6ab81180c35c5f7fe1848da88ff8384c
kimdohyun2 <38269527+kimdohyun2@users.noreply.github.com>
2025-03-26 14:13:09 +0900

    merge: teacher

--- myproject/app/src/main/java/bitcamp/myapp/config/security01/SecurityConfig.java
+++ myproject/app/src/main/java/bitcamp/myapp/config/security01/SecurityConfig.java
@@ -1,10 +1,11 @@
 package bitcamp.myapp.config.security01;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 //@EnableWebSecurity // Spring Boot에서 자동으로 활성화시킨다.
@@ -14,5 +15,22 @@ public class SecurityConfig {
     System.out.println("SecurityConfig 생성자 호출!");
   }
 
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+            .authorizeHttpRequests(auth -> auth
+                    .antMatchers("/auth/login-form",
+                           "/auth/login", "/css/**").permitAll()  // /public/** 경로는 인증 없이 허용
+                    .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                    .loginPage("/auth/login-form")
+                    .loginProcessingUrl("/auth/login")
+                    .usernameParameter("email")
+                    .passwordParameter("password")
+            );
+
+    return http.build();
+  }
   
 }

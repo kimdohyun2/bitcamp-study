7356621c41a988e257fe4fa2b7dd939418f7b09a
kimdohyun2 <38269527+kimdohyun2@users.noreply.github.com>
2025-03-27 14:19:24 +0900

    merge: teacher

---
+++ myproject/app/src/main/java/bitcamp/myapp/config/security03/CustomUserDetails.java
@@ -0,0 +1,54 @@
+package bitcamp.myapp.config.security03;
+
+import bitcamp.myapp.member.Member;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+public class CustomUserDetails implements UserDetails {
+
+    private final Member member;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        ArrayList<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(() -> "ROLE_USER");
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- myproject/app/src/main/java/bitcamp/myapp/member/AuthController.java
+++ myproject/app/src/main/java/bitcamp/myapp/member/AuthController.java
@@ -1,5 +1,7 @@
 package bitcamp.myapp.member;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -11,11 +13,13 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
 
   private MemberService memberService;
+  private static final Log log = LogFactory.getLog(AuthController.class);
 
   public AuthController(MemberService memberService) {
     this.memberService = memberService;
@@ -30,18 +34,17 @@ public class AuthController {
 
   @PostMapping("login")
   public String login(
-          String email,
-          String password,
-          String saveEmail,
-          HttpServletResponse resp,
+          String username,
           HttpSession session) throws Exception {
 
-    Member member = memberService.get(email);
+    log.debug("login() ==================> /auth/login요청 처리!");
+
+    Member member = memberService.get(username);
     if (member == null) {
       return "redirect:login-form";
     }
 
-    if (saveEmail != null) {
+    /*if (saveEmail != null) {
       Cookie emailCookie = new Cookie("email", email);
       emailCookie.setMaxAge(60 * 60 * 24 * 7);
       resp.addCookie(emailCookie);
@@ -49,7 +52,7 @@ public class AuthController {
       Cookie emailCookie = new Cookie("email", "");
       emailCookie.setMaxAge(0);
       resp.addCookie(emailCookie);
-    }
+    }*/
 
     session.setAttribute("loginUser", member);
     return "redirect:/home";
